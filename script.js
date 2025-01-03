26dbcc6faa74f3fde7d2e33c69b48fa30e7272e1
Nikola Vuletić <nikolavuletic412@gmail.com>
2025-01-03 02:08:14 +0100

    Dodane promjene u HTML, CSS i dodan script.js za hamburger meni

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector('.meni');
+    const hamburger = document.querySelector('.hamburger');
+
+    menu.classList.toggle('open');
+    hamburger.classList.toggle('open');
+}
